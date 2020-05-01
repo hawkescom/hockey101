7e0c693e6f06170fd9f540aa23109de4f2922f94
Matt Hawkes <matt.hawkes@livingasone.com>
2020-04-30 20:32:28 -0500

    initial commit

---
+++ src/com/hawkesrules/Main.java
@@ -0,0 +1,30 @@
+package com.hawkesrules;
+
+import java.lang.instrument.ClassDefinition;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+
+        Player player1 = new Player("Josh", "Rhodes", 3, 2, 0, -2);
+        Player player2 = new Player();
+        player2.setFirstName("Brian");
+        player2.setLastName("Weidinger");
+        player2.setNumber(6);
+        player2.setAssists(10);
+        player2.setGoals(59);
+        player2.setPlusMinus(400);
+        player2.calculatePoints();
+
+        Player bestPlaya = null;
+        if (player1.getPoints() > player2.getPoints()) {
+            bestPlaya = player1;
+        } else {
+            bestPlaya = player2;
+        }
+
+        System.out.println ("The player with most points is " + bestPlaya);
+
+    }
+}
---
+++ src/com/hawkesrules/Player.java
@@ -0,0 +1,98 @@
+package com.hawkesrules;
+
+public class Player {
+
+    private String  firstName;
+    private String  lastName;
+    private Integer number;
+    private Integer goals;
+    private Integer assists;
+    private Integer points;
+    private Integer plusMinus;
+
+    public Player(String firstName, String lastName, Integer number, Integer goals, Integer assists, Integer plusMinus) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+        this.goals = goals;
+        this.assists = assists;
+        this.points = goals + assists;
+        this.plusMinus = plusMinus;
+    }
+
+    public Player() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getGoals() {
+        return goals;
+    }
+
+    public void setGoals(Integer goals) {
+        this.goals = goals;
+    }
+
+    public Integer getAssists() {
+        return assists;
+    }
+
+    public void setAssists(Integer assists) {
+        this.assists = assists;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public void calculatePoints() {
+        this.points = this.goals + this.assists;
+    }
+
+    public Integer getPlusMinus() {
+        return plusMinus;
+    }
+
+    public void setPlusMinus(Integer plusMinus) {
+        this.plusMinus = plusMinus;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", number=" + number +
+                ", goals=" + goals +
+                ", assists=" + assists +
+                ", points=" + points +
+                ", plusMinus=" + plusMinus +
+                '}';
+    }
+}
